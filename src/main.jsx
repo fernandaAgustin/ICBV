e38b5e7b0997ed77baf7aa92c8bd87428827b0f2
fernandaAgustin <al222310475@gmail.com>
2025-04-20 21:42:25 -0600

    pagesmode

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PaginaPrincipal from "./components/PaginaPrincipal.jsx";
 import MinisterioJovenes from "./components/MinisterioJovenes.jsx";
-import Conocenos from "./components/Conocenos.jsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<PaginaPrincipal />} />
-      <Route path="/jovenes" element={<MinisterioJovenes />} />  
-      <Route path="/conocenos" element={<Conocenos />} />  
+      <Route path="/jovenes" element={<MinisterioJovenes />} />   
 
 
     </Routes>
